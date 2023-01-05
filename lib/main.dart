557a466694b78796ec5074f45492d2165855bde6
Mikhelmariya <mikhela65@gmail.com>
2023-01-05 21:42:33 +0530

    changed color

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
       home: Scaffold(
-    backgroundColor: Color.fromARGB(255, 218, 243, 230),
+    backgroundColor: Color.fromARGB(255, 228, 244, 236),
     body: Center(
       child: ClipOval(child: Image(image: AssetImage('images/myself.png'))),
     ),
