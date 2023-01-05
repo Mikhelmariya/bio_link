fd3a02cb973a35af268d55f787ab7ad0d489215c
Mikhelmariya <mikhela65@gmail.com>
2023-01-05 22:39:27 +0530

    Row,Column,Containers

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,38 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
     backgroundColor: Color.fromARGB(255, 228, 244, 236),
     body: Center(
-      child: ClipOval(child: Image(image: AssetImage('images/myself.png'))),
+      child: Column(
+        children: [
+          ClipOval(
+            child: Image(
+                image: AssetImage('images/myself.png'),
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Mikhel V Kuttickal',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              Icon(
+                Icons.verified,
+                color: Colors.green,
+                size: 20,
+              )
+            ],
+          )
+        ],
+      ),
     ),
   )));
 }
