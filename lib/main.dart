34c3cd695c6d0cbeb09e6febfdcebeb9196832d0
Mikhelmariya <mikhela65@gmail.com>
2023-01-09 18:17:15 +0530

    Moving to a new website on clicking button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> _launchUrl(String url) async {
+  final Uri _url = Uri.parse(url);
+
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
+  }
+}
 
 void main() {
   runApp(MaterialApp(
@@ -53,6 +59,8 @@ void main() {
           GestureDetector(
             onTap: (() {
               print('Gesture detector onTap called');
+              _launchUrl(
+                  'https://www.linkedin.com/in/mikhel-v-kuttickal-24b760218');
             }),
             child: Container(
               decoration: BoxDecoration(
@@ -74,59 +82,85 @@ void main() {
             ),
           ),
           Spacer(),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(200)),
-              border: Border.all(color: Colors.black, width: 5),
-            ),
-            width: 300,
-            height: 55,
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(Icons.abc_rounded),
-              Spacer(),
-              Text('Discord',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              Spacer(),
-            ]),
-          ),
-          Spacer(),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(200)),
-              border: Border.all(color: Colors.black, width: 5),
-            ),
-            width: 300,
-            height: 55,
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(Icons.abc_rounded),
-              Spacer(),
-              Text('Chatbot',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              Spacer(),
-            ]),
-          ),
-          Spacer(),
-          Container(
-            decoration: BoxDecoration(
+          GestureDetector(
+            onTap: (() {
+              _launchUrl(
+                  'https://drive.google.com/drive/folders/1czcNym8iitFCq5JzP2iDogL1yrl3pCcl?usp=sharing');
+            }),
+            child: Container(
+              decoration: BoxDecoration(
                 color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(200)),
                 border: Border.all(color: Colors.black, width: 5),
-                borderRadius: BorderRadius.all(Radius.circular(200))),
-            width: 300,
-            height: 55,
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Icon(Icons.abc_rounded),
-              Spacer(),
-              Text('Instagram',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              Spacer(),
-            ]),
+              ),
+              width: 300,
+              height: 55,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Icon(Icons.abc_rounded),
+                Spacer(),
+                Text('Certificates',
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Spacer(),
+              ]),
+            ),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: (() {
+              _launchUrl(
+                  'https://drive.google.com/drive/folders/1y_AX7O4ZZTtdF8KEvgb-Rnmo6t_6RJbW?usp=sharing');
+            }),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(200)),
+                border: Border.all(color: Colors.black, width: 5),
+              ),
+              width: 300,
+              height: 55,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Icon(Icons.abc_rounded),
+                Spacer(),
+                Text('NLP Thesis',
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Spacer(),
+              ]),
+            ),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: () {
+              _launchUrl(
+                  'https://drive.google.com/file/d/12ZwyIWKRlvMUelpmNFFAEBejm68doVG2/view?usp=sharing');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 5),
+                  borderRadius: BorderRadius.all(Radius.circular(200))),
+              width: 300,
+              height: 55,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Icon(Icons.abc_rounded),
+                Spacer(),
+                Text('Resume',
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Spacer(),
+              ]),
+            ),
           ),
           Spacer(),
           GestureDetector(
               onTap: () {
                 print('Button clicked');
+                _launchUrl(
+                    'https://drive.google.com/file/d/1Jy78naf1OQqmSi40iMR8aDifNHzL5RLO/view?usp=sharing');
               },
               child: Container(
                 decoration: BoxDecoration(
@@ -140,7 +174,7 @@ void main() {
                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Icon(Icons.abc_rounded),
                   Spacer(),
-                  Text('Twitter',
+                  Text('Computer Vision',
                       style:
                           TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
                   Spacer(),
